f87268c35eaaa102436ae7101e532077627f3907
VictiniX888 <victiniy@gmail.com>
2021-08-18 18:19:43 +0800

    Implement multiple saves

--- src/component/SaveBlock.tsx
+++ src/component/SaveBlock.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/esm/Form';
 import { AppState } from '../App';
 import { createSave, addSave, getSave, loadSave, Saves } from '../save/Save';
 import InputRow from './InputRow';
+import Picker from './Picker';
 
 type SaveBlockProps = {
   appState: AppState;
@@ -15,6 +17,8 @@ type SaveBlockProps = {
 
 type SaveBlockState = {
   saves: Saves;
+  saveInputName: string;
+  selectedSave: string;
 };
 
 class SaveBlock extends React.Component<SaveBlockProps, SaveBlockState> {
@@ -30,39 +34,73 @@ class SaveBlock extends React.Component<SaveBlockProps, SaveBlockState> {
 
     // Initialize saves in memory
     const saves = JSON.parse(window.localStorage.getItem('saves') ?? '{}');
-    this.state = { saves };
+    this.state = { saves, saveInputName: '', selectedSave: '' };
   }
 
-  render() {
-    const saves = this.state.saves;
-    const { appState, setAppState, refreshApp } = this.props;
+  onSaveClick = () => {
+    if (this.state.saveInputName !== '') {
+      const save = createSave(this.state.saveInputName, this.props.appState);
+      const saves = this.state.saves;
+      addSave(save, saves);
+      this.setState({ saves });
+    }
+  };
 
+  onSaveInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ saveInputName: e.target.value });
+  };
+
+  onLoadClick = () => {
+    if (this.state.selectedSave !== '') {
+      const save = getSave(this.state.selectedSave, this.state.saves);
+      if (save !== undefined) {
+        loadSave(save, this.props.setAppState, this.props.refreshApp);
+      }
+    }
+  };
+
+  onLoadInputChange = (value: string) => {
+    this.setState({ selectedSave: value });
+  };
+
+  render() {
     return (
       <div className='input-block'>
         <InputRow>
-          <Button
-            variant='secondary'
-            size='sm'
-            onClick={() => {
-              const save = createSave('test', appState);
-              addSave(save, saves);
-            }}
-          >
+          <Button variant='secondary' size='sm' onClick={this.onSaveClick}>
             Save
           </Button>
 
-          <Button
-            variant='secondary'
+          <Form.Control
+            type='text'
             size='sm'
-            onClick={() => {
-              const save = getSave('test', saves);
-              if (save !== undefined) {
-                loadSave(save, setAppState, refreshApp);
-              }
-            }}
-          >
+            placeholder='Save label (cannot be empty)'
+            onChange={this.onSaveInputChange}
+          />
+        </InputRow>
+
+        <InputRow>
+          <Button variant='secondary' size='sm' onClick={this.onLoadClick}>
             Load
           </Button>
+
+          <Picker
+            id='save-picker'
+            label=''
+            defaultValue=''
+            value={this.state.selectedSave}
+            onChange={this.onLoadInputChange}
+            isLabelShown={false}
+          >
+            <Picker.Item label='' value='' />
+            {Object.values(this.state.saves).map((save) => (
+              <Picker.Item
+                label={save.label}
+                value={save.label}
+                key={save.label}
+              />
+            ))}
+          </Picker>
         </InputRow>
       </div>
     );
